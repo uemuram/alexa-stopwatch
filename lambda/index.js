61ec077986801f45ccb3ddebc9334da157dcfe74
uemuram <uemura.masaru1124@gmail.com>
2021-01-17 22:45:52 +0900

    ファイルサイズ変更

--- lambda/index.js
+++ lambda/index.js
@@ -16,7 +16,7 @@ const SKILL_END = 5;         // スキル終了
 
 // オーディオ関連データ
 const timerSoundUrl_60m = 'https://uemuram.github.io/alexa-stopwatch/timer_60m.mp3';
-const timerSoundUrl_300m = 'https://uemuram.github.io/alexa-stopwatch/timer_300m.mp3';
+const timerSoundUrl_300m = 'https://uemuram.github.io/alexa-stopwatch/timer_240m.mp3';
 const audioMetaData = {
     "title": "計測",
     "subtitle": "「アレクサ、ストップ」で停止",
